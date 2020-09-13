d6c2613aedf9da2142b155f50d31e86b8b673d6b
MathieuProulx <71038288+MathieuProulx@users.noreply.github.com>
2020-09-13 16:06:24 -0400

    MP: fonction square_root et square complété

--- exercice.py
+++ exercice.py
@@ -4,12 +4,12 @@
 
 def square_root(number: int) -> float:
     # TODO completer la fonction
-    return square_rooted
+    return number**0.5
 
 
 def square(number: int) -> int:
     # TODO completer la fonction
-    return squared
+    return number**2
 
 
 def main() -> None:
